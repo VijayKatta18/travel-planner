f5155f8f39fadd09cc15bb30161a97cf0762833f
VijayKatta18 <vijaykatta55@gmail.com>
2025-08-19 23:10:23 +0530

    Day 5 (implmented api calling using fetch method)

--- travel-planner/src/components/Layout.jsx
+++ travel-planner/src/components/Layout.jsx
@@ -1,7 +1,6 @@
 import "./Layout.css"
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import Todo from "./Todo";
 
 export default function Layout({children}){
  return(
--- travel-planner/src/pages/Trips.jsx
+++ travel-planner/src/pages/Trips.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TripCard from "../components/TripCard";
 import WelcomeUser from "../components/WelcomeUser";
-import "./Trips.css"; 
+import "./Trips.css";
 
 export default function Trips() {
-  const [trips, setTrips] = useState([
-    { id: 1, name: "Goa", destination: "Paris" },
-    { id: 2, name: "Dubai", destination: "Dubai Vacation" },
-  ]);
+  const [trips, setTrips] = useState([]);
   const [name, setName] = useState("");
   const [destination, setDestination] = useState("");
+
+  const [loading, setLoading] = useState(true);
 
   function addTrip(e) {
     e.preventDefault();  // prevent page refresh
@@ -23,6 +22,24 @@ export default function Trips() {
     setName("");
     setDestination("");
   }
+
+  // Fetch trips from the APIs
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        const apiTrips = data.map((user) => ({
+          id: user.id,
+          name: user.name,
+          destination: user.address.city
+        }))
+        setTrips(apiTrips);
+        setLoading(false);
+      }).catch((err) => {
+        console.error("error fetching data", err);
+        setLoading(false);
+      })
+  }, []);
 
   return (
     <div>
@@ -57,9 +74,9 @@ export default function Trips() {
 
       {/* Trips List */}
       <div className="trips-list">
-        {trips.map((trip) => (
+        {loading ? ( <span>Loading...</span> ) : ( trips.map((trip) => (
           <TripCard key={trip.id} trip={trip} />
-        ))}
+        )) )}
       </div>
     </div>
   );
