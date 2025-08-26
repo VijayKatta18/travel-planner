8972d1d352123f34d9bd2b970a32ea04b05675d7
VijayKatta18 <vijaykatta55@gmail.com>
2025-08-26 22:54:11 +0530

    updated profile page

--- travel-planner/src/components/Profile.jsx
+++ travel-planner/src/components/Profile.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
 
 export default function Profile() {
-  const user = useSelector((state) => state.auth.user);
 
   return (
     <div>
-      <h1>Profile</h1>
-      <p>Username: {user.username}</p>
-      <p>Status: {user ? "Logged in ✅" : "Guest ❌"}</p>
+      <h1>Profile Details comming Soon</h1>
     </div>
   );
 }
