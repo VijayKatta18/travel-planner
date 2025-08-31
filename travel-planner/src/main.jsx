bfbfa64231600f229fee0dde24debf23ecd0c22a
VijayKatta18 <vijaykatta55@gmail.com>
2025-08-31 21:56:52 +0530

    Added git ignore

--- travel-planner/src/main.jsx
+++ travel-planner/src/main.jsx
@@ -1,14 +1,20 @@
+
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
+const googleClientId =  import.meta.env.VITE_GOOGLE_CLIENT_ID;;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <App />
+      </GoogleOAuthProvider>
     </BrowserRouter>
   </Provider>
 )
